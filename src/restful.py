ef34400fc9e83e7c86cfbc307e0b0f0b6f4b00d8
william <william@twroa.com>
2024-01-03 16:49:16 +0800

    remove oidc

--- src/restful.py
+++ src/restful.py
@@ -1,8 +1,6 @@
-import os
 
-import jwt
 import python_libs.vault_to_k8s as vault_to_k8s
-from fastapi import FastAPI, HTTPException, Request, status
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel
 from python_libs import jlogger
@@ -19,29 +17,15 @@ class Resource(BaseModel):
     source_kv2_name: str
     source_kv2_path: str
 
-def jwt_token_check(request: Request):
-    oidc_enabled = os.getenv("oidc_enabled", "false").lower()
-    if oidc_enabled == 'true':
-        try:
-            token = request.headers['x-forwarded-access-token']
-            return jwt.decode(token, options={"verify_signature": False}, algorithms=['RS256'])
-        except (KeyError, jwt.exceptions.PyJWTError):
-            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Missing access token.")
-
-@app.get("/")
-def root():
-    return {"status": "ok"}
-
-@app.get("/_info/")
+@app.get("/_info")
 def info_get():
     return {
         "vault_info": vault_to_k8s.vault_api.info(),
         "k8s_info": vault_to_k8s.k8s_api.get_cluster_info()
     }
 
-@app.post("/resource/")
-def resource_create(resource: Resource, request: Request):
-    jwt_token_check(request)
+@app.post("/resource")
+def resource_create(resource: Resource):
     try:
         vault_to_k8s.create_k8s_resource(resource.model_dump())
         return JSONResponse(content={"status": "successful"})
