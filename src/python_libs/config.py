b0ea836413b803b7d5845214944c99bf08296d93
william <william@twroa.com>
2024-01-10 16:56:16 +0800

    update k8s manifests admission policy set to ignore http server log set to warning remove approle secret update env name

--- src/python_libs/config.py
+++ src/python_libs/config.py
@@ -1,5 +1,8 @@
 import os
 
+from python_libs import jlogger
+
+logger = jlogger.Jloger()
 
 class Appconfig:
     _instance = None
@@ -9,18 +12,21 @@ class Appconfig:
         return cls._instance
 
     def __init__(self):
-        self.in_develop = os.getenv('in_develop', 'false')
-        self.annotation_prefix = os.getenv('annotation_prefix', 'vaultpy.io')
-        self.kubeconfig = os.getenv('KUBECONFIG')
-        self.vault_scheme = os.getenv("vault_scheme")
-        self.vallt_fqdn = os.getenv("valult_fqdn")
+        self.in_develop = os.getenv('DEV_MODE', 'false')
+        self.annotation_prefix = os.getenv('K8S_ANNOTATION_PREFIX', 'vaultpy.io')
+        self.kubeconfig = os.getenv('K8S_CONFIG')
+        self.vault_scheme = os.getenv("VAULT_SCHEME")
+        self.vallt_fqdn = os.getenv("VAULT_FQDN")
         self.vault_auth_method_name = os.getenv('VAULT_AUTH_METHOD', 'k8s')
-        self.vault_auth_approle_id = os.getenv("role_id")
-        self.vault_auth_approle_secret_id  = os.getenv("secret_id")
+        self.vault_auth_approle_id = os.getenv("VAULT_AUTH_APPROLE_ID")
+        self.vault_auth_approle_secret_id  = os.getenv("VAULT_AUTH_APPROLE_SECRET_ID")
         self.vault_auth_k8s_path = os.getenv('VAULT_AUTH_K8S_PATH')
         self.vault_auth_k8s_role = os.getenv('VAULT_AUTH_K8S_ROLE')
         if self.in_develop == 'true':
             self.vault_auth_k8s_token = os.getenv('VAULT_AUTH_K8S_TOKEN')
         else:
-            with open('/var/run/secrets/kubernetes.io/serviceaccount/token') as sa_token:
-                self.vault_auth_k8s_token = sa_token.read()
+            try:
+                with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as sa_token:
+                    self.vault_auth_k8s_token = sa_token.read()
+            except Exception as e:
+                logger.error(str(e))
