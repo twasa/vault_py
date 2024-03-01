0dfee8d1eb4489d9ecbf5bbbe2583da02b0b5091
william <william@twroa.com>
2024-03-01 15:18:18 +0800

    http request add no-cache header

--- src/python_libs/tools_http.py
+++ src/python_libs/tools_http.py
@@ -7,35 +7,43 @@ from python_libs import jlogger
 
 logger = jlogger.Jloger()
 
+
 def http_get(url: str, headers: dict[str, str] = None, **kwargs):
     http_session = requests.Session()
     if headers:
         http_session.headers.update(headers)
-    response = http_session.get(url=url)
+    response = http_session.get(url=url, headers={"Cache-Control": "no-cache"})
     if not response.status_code == 200:
-        message = f'http request error, url: {url}, code: {response.status_code}, message: {response.text}'
+        message = f"http request error, url: {url}, code: {response.status_code}, message: {response.text}"
         logger.error(message)
     return response
 
-def http_put(url: str, json_data: dict[str, str], headers: dict[str, str] = None, **kwargs):
+
+def http_put(
+    url: str, json_data: dict[str, str], headers: dict[str, str] = None, **kwargs
+):
     http_session = requests.Session()
     if headers:
         http_session.headers.update(headers)
     response = http_session.put(url=url, json=json_data)
     if not response.status_code == 200:
-        message = f'http request error, url: {url}, code: {response.status_code}, message: {response.text}'
+        message = f"http request error, url: {url}, code: {response.status_code}, message: {response.text}"
         logger.error(message)
     return response
 
-def http_post(url: str, json_data: dict[str, str], headers: dict[str, str] = None, **kwargs):
+
+def http_post(
+    url: str, json_data: dict[str, str], headers: dict[str, str] = None, **kwargs
+):
     http_session = requests.Session()
     if headers:
         http_session.headers.update(headers)
     response = http_session.post(url=url, json=json_data)
     if not response.status_code == 200:
-        message = f'http request error, url: {url}, code: {response.status_code}, message: {response.text}'
+        message = f"http request error, url: {url}, code: {response.status_code}, message: {response.text}"
         logger.error(message)
     return response
+
 
 def https_der_cert_bin_retrieve(fqdn: str) -> str:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -47,11 +55,13 @@ def https_der_cert_bin_retrieve(fqdn: str) -> str:
     wrappedSocket.close()
     return der_cert_bin
 
+
 def cert_bin_to_pem(cert_bin: bytes) -> str:
     pem_cert = ssl.DER_cert_to_PEM_cert(cert_bin)
     return pem_cert
 
-def https_fingerprint_retrieve(der_cert_bin: bytes, hash_type: str = 'sha1') -> str:
+
+def https_fingerprint_retrieve(der_cert_bin: bytes, hash_type: str = "sha1") -> str:
     hash_obj = getattr(hashlib, hash_type)
     thumb = hash_obj(der_cert_bin).hexdigest()
     return thumb
